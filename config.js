dfcd04a1588a3e693f3a46fb38e54b02de96993b
Monika Radosiewicz <monika.radosiewicz@gmail.com>
2018-05-26 23:35:22 +0200

    init repository

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '40244842940-n4185h5dtroja8ph45bb013kravffcsq.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': '_rIjkUhuGLCxwrxJxyGDzo7L' ,
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+
+};
